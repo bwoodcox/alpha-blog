243d436dd70a5de4ff650045fa040cede1136f8e
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-09 15:48:39 -0600

    Add user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_hash = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar/#{email_hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
