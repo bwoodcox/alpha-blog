1af5f6b0e92ae3c50d5b2dd4eb2120a6bd188373
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-10 18:11:24 -0600

    Add authentication process and updated nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.username}, welcome to the AlphaBlog!"
       redirect_to articles_path
     else
@@ -23,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your profile was successfully updated."
       redirect_to @user
@@ -39,5 +39,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
