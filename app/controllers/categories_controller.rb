4b95e711ba42a3ee9d1a021d8a227aa45b88fef3
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-11 23:21:33 -0600

    Add categories feature, unit, functional & integration tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was saved successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
